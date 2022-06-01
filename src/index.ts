65fceb04fa1e938d1cb4489e95fb9d6d37e304a4
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-06-01 05:13:25 +0200

    Register

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express, {Request, Response} from 'express';
 import {createConnection} from "typeorm";
+import {routes} from "./routes";
 
 createConnection().then(() => {
     const app = express();
 
     app.use(express.json());
 
-    app.get('/', (req: Request, res: Response) => {
-        res.send('Hello');
-    });
+   routes(app);
 
     app.listen(8001, () => {
         console.log('listening to port 8001')
