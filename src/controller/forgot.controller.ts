e4c8e9e917cca345c33abec152b0faceb4fc1e99
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-06-06 21:01:34 +0200

    Sending Emails

--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -1,16 +1,33 @@
 import {Request, Response} from "express";
 import {Reset} from "../entity/reset.entity";
 import {getRepository} from "typeorm";
+import {createTransport} from "nodemailer";
 
 
 export const Forgot = async (req: Request, res: Response) => {
     const {email} = req.body;
     const token = Math.random().toString(20).substring(2, 12);
 
-    const reset = await getRepository(Reset).save({
+    await getRepository(Reset).save({
         email,
         token
     })
 
-    res.send(reset);
+    const transporter = createTransport({
+        host: '0.0.0.0',
+        port: 1025
+    })
+
+    const url = `http://localhost:3000/reset/${token}`;
+
+    await transporter.sendMail({
+        from: 'from@examlpe.com',
+        to: email,
+        subject: 'Reset your password!',
+        html: `Click <a href="${url}">here</a> to reset your password!`
+    })
+
+    res.send({
+        message: 'Please check your email!'
+    });
 }
