8ba3605fa34fbf84c9ed600c43fa803331240b9f
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-06-07 07:47:29 +0200

    Reset Password

--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -2,9 +2,11 @@ import {Request, Response} from "express";
 import {Reset} from "../entity/reset.entity";
 import {getRepository} from "typeorm";
 import {createTransport} from "nodemailer";
+import {User} from "../entity/user.entity";
+import bcryptjs from "bcryptjs";
 
 
-export const Forgot = async (req: Request, res: Response) => {
+export const ForgotPassword = async (req: Request, res: Response) => {
     const {email} = req.body;
     const token = Math.random().toString(20).substring(2, 12);
 
@@ -30,4 +32,38 @@ export const Forgot = async (req: Request, res: Response) => {
     res.send({
         message: 'Please check your email!'
     });
+}
+
+export const ResetPassword = async (req: Request, res: Response) => {
+    const {token, password, password_confirm} = req.body;
+
+    if (password !== password_confirm) {
+        return res.status(400).send({
+            message: "Passwords do not match!"
+        });
+    }
+
+    const resetPassword = await getRepository(Reset).findOneBy({token});
+
+    if (!resetPassword) {
+        return res.status(400).send({
+            message: "Invalid url!"
+        });
+    }
+
+    const user = await getRepository(User).findOneBy({email: resetPassword.email})
+
+    if (!user) {
+        return res.status(404).send({
+            message: "User not found!"
+        });
+    }
+
+    await getRepository(User).update(user.id, {
+        password: await bcryptjs.hash(password, 12)
+    });
+
+    res.send({
+        message: 'success'
+    })
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express'
 import {AuthenticateUser, Login, Logout, Refresh, Register} from "./controller/auth.controller";
-import {Forgot} from "./controller/forgot.controller";
+import {ForgotPassword, ResetPassword} from "./controller/forgot.controller";
 
 export const routes = (router: Router) => {
     router.post('/api/register', Register);
@@ -8,5 +8,7 @@ export const routes = (router: Router) => {
     router.get('/api/user', AuthenticateUser);
     router.post('/api/refresh', Refresh);
     router.post('/api/logout', Logout);
-    router.post('/api/forgot', Forgot);
+
+    router.post('/api/forgot', ForgotPassword);
+    router.post('/api/reset', ResetPassword);
 }
